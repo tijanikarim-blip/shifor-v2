503f133790249e13ff6ef769c3f0a3787c1bbbaf
Tijan Karim <tijanikarim.blip@gmail.com>
2026-04-25 21:07:03 +0000

    Fix typo: ios to iOS in firebase_options

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -11,7 +11,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        return ios;
+        return iOS;
       case TargetPlatform.macOS:
         return macOS;
       case TargetPlatform.windows:
