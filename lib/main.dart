598f3f24e6b8d1d8ae7753cd761f93c7d7c2e079
Tijan Karim <tijanikarim.blip@gmail.com>
2026-04-26 17:54:27 +0000

    Fix v2 embedding

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text('Shifor Works!'),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const ShiforApp());
+}
+
+class ShiforApp extends StatelessWidget {
+  const ShiforApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shifor',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88E5)),
       ),
-    ),
-  ));
+      home: const Scaffold(
+        body: Center(
+          child: Text('Shifor Works!', style: TextStyle(fontSize: 32)),
+        ),
+      ),
+    );
+  }
 }
